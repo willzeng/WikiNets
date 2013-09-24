fb408544e9f2dd55d99b2019219045bc20db242c
backens <backens@cs.fake>
2013-09-24 14:34:03 +0100

    Simplified code for creating & editing; added comments.

--- static/wikiscript.js
+++ static/wikiscript.js
@@ -1,3 +1,4 @@
+// toggles whether the given layer is visible
 function showlayer(layer){
   var myLayer = document.getElementById(layer);
   if(myLayer.style.display=="none" || myLayer.style.display==""){
@@ -14,10 +15,17 @@ function showlayer(layer){
 // this is currently a bit of a hack of jQuery and non-jQuery code.
 // Not sure how to convert it all into jQuery though...
 // Code for adding more input fields modified from http://www.quirksmode.org/dom/domform.html
+
+// global variables
+// - "counter" is used to keep track of the input fields generated on-the-fly
+// - "selected_node" keeps track of which node is selected for editing
+// - "reserved_keys" is a global constant and keeps track of property names
+//   which are reserved for system use
 var counter = 0;
 var selected_node;
 var reserved_keys = ["_id"];
 
+// adds pairs of input fields for properties and values
 function moreFields(writediv, rootdiv, classNamediv) {
   counter++;
   var newFields = document.getElementById(rootdiv).cloneNode(true);
@@ -34,6 +42,10 @@ function moreFields(writediv, rootdiv, classNamediv) {
   insertHere.parentNode.insertBefore(newFields,insertHere);
 }
 
+// selects a node for editing
+// called either by clicking on a node in the visualisation while the
+// "edit node" menu is open or by entering a number into the "select node"
+// input field and then clicking the "select node" button
 function select_node(nodeid) {
     $.post('/get_id', {nodeid: nodeid}, function(data) {
       if (data == "error") {
@@ -57,27 +69,59 @@ function select_node(nodeid) {
     });
 }
 
-function is_legal(property) {
-  // ensures that property names won't break the cypher queries
+
+// checks whether property names will break the cypher queries or are any of
+// the reserved terms
+function is_illegal(property, type) {
   if (property == '') { 
-    alert("Property names must not be empty.");
-    return false;
-  } else if (/^.*[^a-zA-Z0-9_].*$/.test(property)) {
-    alert("Property name '" + property + "' illegal: property names must only contain alphanumeric characters and underscore.");
-    return false;
-  } else if (reserved_keys.indexOf(property) != -1) {
-    alert("Property name illegal: '" + property + "' is a reserved term.");
-    return false;
-  } else {
+    alert(type + " name must not be empty.");
     return true;
+  } else if (/^.*[^a-zA-Z0-9_].*$/.test(property)) {
+    alert(type + " name '" + property + "' illegal: " + type + " names must only contain alphanumeric characters and underscore.");
+    return true;
+  } else if (reserved_keys.indexOf(property) != -1) {
+    alert(type + " name illegal: '" + property + "' is a reserved term.");
+    return true;
+  } else {
+    return false;
   };
 }
 
 
+// takes a form and populates a propertyObject with the property-value pairs
+// contained in it, checking the property names for legality in the process
+// returns: submitOK: a boolean indicating whether the property names were all
+//                    legal
+//          propertyObject: a dictionary of property-value pairs
+function assign_properties(form_name) {
+    var submitOK = true;
+    var propertyObject = {};
+    $("." + form_name + "Property").each(function(i, obj) {
+      var property = $(this).children(".property" + form_name).val();
+      var value = $(this).children(".value" + form_name).val();
+      // check whether property name is allowed and ensure that user does not
+      // accidentally assign the same property twice
+      // - if property name is not ok, there is an apropriate error message and
+      //   node creation is cancelled
+      // - if property name is ok, property-value pair is assigned to the
+      //   nodeObject, escaping any single quotes in the value so they don't
+      //   break the cypher query
+      if (is_illegal(property, "Property")) {
+        submitOK = false;
+      } else if (property in propertyObject) {
+        alert("Property '" + property + "' already assigned.\nFirst value: " + propertyObject[property] + "\nSecond value: " + value);
+        submitOK = false;
+      } else {
+        propertyObject[property] = value.replace(/'/g, "\\'");
+      };
+    });
+    return [submitOK, propertyObject];
+}
+
+// this is the interactive stuff that happens after the document has loaded
 $(document).ready(function(){
 
-  // there should probably be a more elegant way of doing all of this
-  // menu showing/hiding, but I haven't figured it out yet.
+  // showing or hiding various menus and submenus
   $("img.choose_menu").click(function() {
     showlayer('browse_menu');
     showlayer('edit_menu');
@@ -103,24 +147,13 @@ $(document).ready(function(){
     showlayer('sm_3');
   });
 
-
+  // create a new node from the data in the create node input form
   $("#createObj").on("click", function(event){
-    var nodeObject = {};
-    var submitOK = true;
-    $('.nodeProperty').each(function(i, obj) {
-      var property = $(this).children(".propertyObj").val();
-      var value = $(this).children(".valueObj").val();
-      if (!(is_legal(property))) {
-        submitOK = false;
-        return false;
-      } else if (property in nodeObject) {
-        alert("Property '" + property + "' already assigned.\nFirst value: " + nodeObject[property] + "\nSecond value: " + value);
-        submitOK = false;
-        return false;
-      } else {
-        nodeObject[property] = value.replace(/'/g, "\\'");
-      };
-    });
+    var submitOK, nodeObject;
+    // check property names and assign property-value pairs to nodeObject
+    [submitOK, nodeObject] = assign_properties("Node");
+    // if all property names were fine, remove the on-the-fly created input
+    // fields and submit the data to the server to actually create the node
     if (submitOK) {
       $('.nodeProperty').each(function(i, obj) {
         $(this)[0].parentNode.removeChild($(this)[0]);
@@ -128,13 +161,18 @@ $(document).ready(function(){
       console.log(JSON.stringify(nodeObject));
       $.post('/create_node', nodeObject, function(data) {
         alert("Created node with ID " + data);
-        // would now like to reload the visualisation here
+        // would now like to have option of reloading the visualisation here
       });
     }
   });
 
-
+  // creates a relationship from the data in the create-relationship input form
   $("#createArr").on("click", function(event){
+    // relationships must have a beginning and end node (given by ID) and a
+    // type; beginning and end node IDs must be numbers; relationship type
+    // must obey same rules as property names
+    // if any of these conditions are not satisfied, the user is informed and
+    // relationship creation is cancelled
     if (!(/^[0-9]+$/.test($("#from").val()))) {
       alert("'From: node ID' must be a number.")
       return false;
@@ -143,49 +181,41 @@ $(document).ready(function(){
       alert("'To: node ID' must be a number.")
       return false;
     };
-    if (/^.*[^a-zA-Z0-9_].*$/.test($("#rel-type").val())) {
-      alert("Relationship type '"+ $("#rel-type").val() + "' illegal: relationship types must only contain alphanumeric characters and underscore.")
+    if (is_illegal($("#rel-type").val(), "Relationship type")) {
       return false;
     };
     var relObject = {from: $("#from").val(),
                      to: $("#to").val(),
                      type: $("#rel-type").val()};
-    var relProperties = {};
-    var submitOK = true;
-    $('.relProperty').each(function(i, obj) {
-      var property = $(this).children(".propertyArr").val();
-      var value = $(this).children(".valueArr").val();
-      if (!(is_legal(property))) {
-        submitOK = false;
-        return false;
-      } else if (property in relProperties) {
-        alert("Property '" + property + "' already assigned.\nFirst value: " + relProperties[property] + "\nSecond value: " + value);
-        submitOK = false;
-        return false;
-      } else {
-        relProperties[property] = value.replace(/'/g, "\\'");
-      };
-    });
-    relObject["properties"] = relProperties;
+    var submitOK, relProperties;
+    // check property names and assign property-value pairs to relProperties
+    [submitOK, relProperties] = assign_properties("Arr");
+    // if all is well, send data to server
     if (submitOK) {
+      relObject["properties"] = relProperties;
       console.log(JSON.stringify(relObject));
       $.post('/create_rel', relObject, function(data) {
         if (data == "error") {
+          // most likely cause for errors is trying to create a relationship
+          // where one of the end nodes does not exist
           alert("An error occured. Please check whether nodes with the given IDs (" + $("#from").val() + ", " + $("#to").val() + ") exist.");
         } else {
+          // if relationship has been created successfully, remove the on-the-
+          // fly generated input fields
           $('.relProperty').each(function(i, obj) {
             $(this)[0].parentNode.removeChild($(this)[0]);
           });
           alert("Created relationship with ID " + data + ".");
-          // would now like to reload the visualisation here
+          // would now like option to reload the visualisation here
         };
       });
     }
   });
 
 
-
+  // selects a node for editing from the node ID input field
   $("#SelectNode").on("click", function(event) {
+    // check whether node ID is a number
     if (!(/^[0-9]+$/.test($("#SelectNodeID").val()))) {
       alert("Node ID must be a number.");
     } else {
@@ -193,31 +223,20 @@ $(document).ready(function(){
     };
   });
 
+
+  // saves new properties for a node
   $("#EditNode").on("click", function(event){
-    var nodeObject = {};
-    var submitOK = true;
-    $('.EditProperty').each(function(i, obj) {
-      var property = $(this).children(".propertyEdit").val();
-      var value = $(this).children(".valueEdit").val();
-      if (!(is_legal(property))) {
-        submitOK = false;
-        return false;
-      } else if (property in nodeObject) {
-        alert("Property '" + property + "' already assigned.\nFirst value: " + nodeObject[property] + "\nSecond value: " + value);
-        submitOK = false;
-        return false;
-      } else {
-        nodeObject[property] = value.replace(/'/g, "\\'");
-      };
-    });
+    var submitOK, nodeObject;
+    // assign property-value pairs to nodeObject and check for legality
+    [submitOK, nodeObject] = assign_properties("Edit");
     if (submitOK) {
-      $('.EditProperty').each(function(i, obj) {
-        $(this)[0].parentNode.removeChild($(this)[0]);
-      });
-      console.log(JSON.stringify(nodeObject));
       if (JSON.stringify(nodeObject) == '{}') {
         alert("No properties are being set for node " + selected_node + ".");
       } else {
+        $(".EditProperty").each(function(i, obj) {
+          $(this)[0].parentNode.removeChild($(this)[0]);
+        });
+        console.log(JSON.stringify(nodeObject));
         $.post('/edit_node', {nodeid: selected_node, properties: nodeObject}, function(data) {
           alert("Saved changes to node " + selected_node + ".");
         });
