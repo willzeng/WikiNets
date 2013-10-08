3bf1f96b1f5091acc96fa9f17c21ae6efbadaf44
Joe Henke <jdhenke@mit.edu>
2013-10-07 21:52:59 -0400

    make npm test work locally

---
+++ test/appSpec.js
@@ -0,0 +1,12 @@
+define(['jquery', 'underscore'], function($, _) {
+
+    describe('just checking', function() {
+
+        it('works for underscore', function() {
+            // just checking that _ works
+            expect(_.size([1,2,3])).toEqual(3);
+        });
+
+    });
+
+});
