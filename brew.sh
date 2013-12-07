3abe4aa0053934b07f802ec20dcb3a6134943511
willzeng <zeng.will@gmail.com>
2013-12-07 22:39:00 +0000

    update brew

--- brew.sh
+++ brew.sh
@@ -2,7 +2,6 @@
 
 # compile the coffeescript files in this example project
 coffee --compile static/main.coffee &&\
-coffee --compile static/WikiNetsDataProvider.coffee &&\
 
 # compile the coffeescript files in celestrium
 coffee --compile -o static/celestrium_code/core/ static/celestrium_code/core-coffee/ 
