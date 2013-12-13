7a92d7c30f745137f38ff5d9f040e5d65dac4c7f
willzeng <zeng.will@gmail.com>
2013-12-13 23:41:35 +0000

    setup for heroku

---
+++ heroku.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# compile the coffeescript files in this example project
+coffee --compile static/*.coffee &
+
+# compile the coffeescript files in celestrium
+coffee --compile -o static/celestrium_code/core/ static/celestrium_code/core-coffee/ &
+
+# statically serve files out of ./www/
+node web.js
