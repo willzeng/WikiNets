5e5aaa42fe0f12ec1cbf1f6ba8b2a9d11dbd8b16
willzeng <zeng.will@gmail.com>
2013-12-27 16:41:19 -0500

    add string

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ require('coffee-script');
 
 //var url = process.env.NEO4J_URL || 'http://localhost:7474';
 //var url = 'http://wikinets-edge:wiKnj2gYeYOlzWPUcKYb@wikinetsedge.sb01.stations.graphenedb.com:24789';
-var url = 'wikinets-shoebox:ofzn7wT7CrrrSDLkXrog@http://wikinetsshoebox.sb01.stations.graphenedb.com:24789';
+var url = 'http://wikinets-shoebox:ofzn7wT7CrrrSDLkXrog@http://wikinetsshoebox.sb01.stations.graphenedb.com:24789';
 
 var neo4js = require('neo4js');
 
