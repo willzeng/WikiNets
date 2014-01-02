ad0d711acc707f3d18a58b18b07f818f0f68a545
willzeng <zeng.will@gmail.com>
2014-01-02 15:34:00 -0500

    set target to grapheneDB wikinets-demo

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ require('coffee-script');
 
 //var url = process.env.NEO4J_URL || 'http://localhost:7474';
 //var url = 'http://wikinets-edge:wiKnj2gYeYOlzWPUcKYb@wikinetsedge.sb01.stations.graphenedb.com:24789';
-var url = 'http://wikinets-shoebox:ofzn7wT7CrrrSDLkXrog@wikinetsshoebox.sb01.stations.graphenedb.com:24789';
+var url = 'http://wikinets-demo:BEmJ3fqsO02bHl9xay7X@wikinetsdemo.sb01.stations.graphenedb.com:24789';
 
 var neo4js = require('neo4js');
 
