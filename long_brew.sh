d146ef5dfe161fe46acdda05d4d99e726b3a8a8e
willzeng <zeng.will@gmail.com>
2014-01-04 08:52:34 -0500

    cleanup files

--- long_brew.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# compile the coffeescript files in this example project
-coffee --watch --compile static/*.coffee &
-
-# compile the coffeescript files in celestrium
-coffee --watch --compile -o static/celestrium_code/core/ static/celestrium_code/core-coffee/ 
