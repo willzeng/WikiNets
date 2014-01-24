ad161599e43b0f009e30e08af27831b15cc5b01e
CalvinFong <calvin_c_fong@hotmail.com>
2014-01-24 14:08:17 +0000

    target demo database

--- web.js
+++ web.js
@@ -4,6 +4,12 @@ require('coffee-script');
 //var url = 'http://wikinets-edge:wiKnj2gYeYOlzWPUcKYb@wikinetsedge.sb01.stations.graphenedb.com:24789';
 var url = 'http://wikinets-demo:BEmJ3fqsO02bHl9xay7X@wikinetsdemo.sb01.stations.graphenedb.com:24789';
 
+//sosi
+//var url = 'http://wikinets-sosi:yKenjgdUuhYXpcjevdNk@wikinetssosi.sb01.stations.graphenedb.com:24789';
+//pims
+//var url = 'http://wikinets-pims:GH9I6tXib6Zl9K5RmQBR@wikinetspims.sb01.stations.graphenedb.com:24789';
+
+
 var neo4js = require('neo4js');
 
 var graphDb = new neo4js.GraphDatabase4Node(url);
